fe5770a66bcf8ee11c659546d378ca3d079b21a0
cmolinan <cmolinan@gmail.com>
2022-08-26 08:35:07 -0500

    update of DOM folder

--- src/index.js
+++ src/index.js
@@ -4,18 +4,10 @@ import DOM from './modules/dom.js';
 
 const dom = new DOM();
 
-const addScoreButtonListener = () => {
-  dom.addScoreForm.addEventListener('click', dom.addNewScore);
-};
-
-const refreshListButtonListener = () => {
-  dom.refreshListButton.addEventListener('click', dom.refreshScoresList);
-};
-
 // INITS
 function init() {
-  addScoreButtonListener(); // Listener for Submit Form
-  refreshListButtonListener(0); // Listener for Refresh Button
+  dom.addScoreButtonListener(); // Listener for Submit Form
+  dom.refreshListButtonListener(0); // Listener for Refresh Button
   dom.refreshScoresList(0); // Load the HTML list
 }
 
